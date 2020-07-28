43816eb32bb03ca635398ade4bfdd7b5efb7e60e
Ivan Petrov <ivan.petrov@jetbrains.com>
2020-07-28 11:34:33 +0300

    add lambda deployment files
    GitOrigin-RevId: 55dfcf376bdbf252e2f795c5d025352e8bd98144

--- lambda-wrapper/build.gradle.kts
+++ lambda-wrapper/build.gradle.kts
@@ -15,16 +15,16 @@ tasks.withType<KotlinCompile> {
 }
 
 dependencies {
-  implementation("com.amazonaws:aws-lambda-java-log4j:1.0.0")
-  implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
-  implementation("org.slf4j:slf4j-log4j12:1.7.30")
+  implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${rootProject.properties["jacksonVersion"]}")
   implementation("software.amazon.awssdk:url-connection-client:2.10.78")
   implementation(project(":services:plugin-blockmap-patches"))
+  runtimeOnly("org.apache.logging.log4j:log4j-slf4j18-impl:2.13.0")
+  runtimeOnly("com.amazonaws:aws-lambda-java-log4j2:1.2.0")
 }
 
 
-buildDir = File(rootProject.projectDir, "build/plugin-blockmap-patches")
+buildDir = File(rootProject.projectDir, "build/plugin-blockmap-lambda")
 
 tasks.register<Zip>("buildPreviewZip") {
   from(project.tasks["compileKotlin"])
