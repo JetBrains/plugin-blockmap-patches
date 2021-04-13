2a263b85619503b11fdc4f45b7e0695c607deb24
Ivan Chirkov <Ivan.Chirkov@jetbrains.com>
2021-04-13 12:28:00 +0200

    MavenCentral publishing fixes
    GitOrigin-RevId: 5d17fb3bf7ca8a4e7850a3992f5c94bd91ac6298

--- blockmap/build.gradle.kts
+++ blockmap/build.gradle.kts
@@ -18,58 +18,54 @@ java {
   withJavadocJar()
 }
 
-signing {
-  isRequired = hasProperty("mavenCentralUsername")
-
-  useInMemoryPgpKeys(findProperty("signingKey").toString(), findProperty("signingPassword").toString())
-  sign(publishing.publications["blockmap-maven"])
-}
-
 publishing {
   publications {
+    fun MavenPublication.configurePom() {
+      pom {
+        name.set("JetBrains Blockmap")
+        description.set("Library to chunk files and build metadata for patch updates. Based on the FastCDC algorithm.")
+        url.set("https://github.com/JetBrains/plugin-blockmap-patches")
+        licenses {
+          license {
+            name.set("The Apache Software License, Version 2.0")
+            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+        developers {
+          developer {
+            id.set("chrkv")
+            name.set("Ivan Chirkov")
+            organization.set("JetBrains")
+          }
+          developer {
+            id.set("satamas")
+            name.set("Semyon Atamas")
+            organization.set("JetBrains")
+          }
+          developer {
+            id.set("AlexanderPrendota")
+            name.set("Alexander Prendota")
+            organization.set("JetBrains")
+          }
+          developer {
+            id.set("TAJlOS")
+            name.set("Ivan Petrov")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/JetBrains/plugin-blockmap-patches.git")
+          developerConnection.set("scm:git:ssh://github.com/JetBrains/plugin-blockmap-patches.git")
+          url.set("https://github.com/JetBrains/plugin-blockmap-patches")
+        }
+      }
+    }
+
     create<MavenPublication>("blockmap-maven") {
       groupId = "org.jetbrains.intellij"
       artifactId = "blockmap"
       version = project.version.toString()
       from(project(":services:plugin-blockmap-patches:blockmap").components["java"])
-    }
-
-    pom {
-      name.set("JetBrains Blockmap")
-      description.set("Library to chunk files and build metadata for patch updates. Based on the FastCDC algorithm.")
-      url.set("https://github.com/JetBrains/plugin-blockmap-patches")
-      licenses {
-        license {
-          name.set("The Apache Software License, Version 2.0")
-          url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-        }
-      }
-      developers {
-        developer {
-          id.set("chrkv")
-          name.set("Ivan Chirkov")
-          organization.set("JetBrains")
-        }
-        developer {
-          id.set("satamas")
-          name.set("Semyon Atamas")
-          organization.set("JetBrains")
-        }
-        developer {
-          id.set("AlexanderPrendota")
-          name.set("Alexander Prendota")
-          organization.set("JetBrains")
-        }
-        developer {
-          id.set("TAJlOS")
-          name.set("Ivan Petrov")
-        }
-      }
-      scm {
-        connection.set("scm:git:git://github.com/JetBrains/plugin-blockmap-patches.git")
-        developerConnection.set("scm:git:ssh://github.com/JetBrains/plugin-blockmap-patches.git")
-        url.set("https://github.com/JetBrains/plugin-blockmap-patches")
-      }
+      configurePom()
     }
   }
 
@@ -83,4 +79,11 @@ publishing {
       }
     }
   }
+}
+
+signing {
+  isRequired = hasProperty("mavenCentralUsername")
+
+  useInMemoryPgpKeys(findProperty("signingKey").toString(), findProperty("signingPassword").toString())
+  sign(publishing.publications["blockmap-maven"])
 }
